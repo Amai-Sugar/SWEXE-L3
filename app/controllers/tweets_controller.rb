a999b4ec1430a04bb190291a01efd78ff0275f30
Ubuntu <ubuntu@ip-172-31-88-171.ec2.internal>
2020-10-02 14:54:29 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,49 @@
+class TweetsController < ApplicationController
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    params[:tweet][:tdate] = Time.current
+    @tweet = Tweet.new(message: params[:tweet][:message], tdate: params[:tweet][:tdate])
+    if @tweet.save
+      flash[:notice] = "ツイートしました"
+      redirect_to '/'
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(message: params[:tweet][:message])
+    flash[:notice] = "ツイートを編集しました"
+    redirect_to '/'
+  end
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash[:notice] = "ツイートを削除しました"
+    redirect_to '/'
+  end
+  
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+  
+end
